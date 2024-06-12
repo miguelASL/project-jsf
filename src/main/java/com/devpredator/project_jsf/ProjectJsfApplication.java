3cad3e1ffe6d74118f859bbffd58ef564f302920
Mike Sarmiento <msarmientolevy@gmail.com>
2024-06-12 21:07:53 +0200

    version inicial del proyecto project-jsf

---
+++ src/main/java/com/devpredator/project_jsf/ProjectJsfApplication.java
@@ -0,0 +1,13 @@
+package com.devpredator.project_jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpredator/project_jsf/ProjectJsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpredator.project_jsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
